73cd96095db390d4edec86f383fdfe8a613a434f
Tyler Stephenson <tylerstephenson@tylers-mbp.lan>
2022-07-30 18:28:09 -0400

    add dev function

---
+++ groupme-listener/push-dev/__init__.py
@@ -0,0 +1,33 @@
+import logging
+import webbrowser
+import azure.functions as func
+import os
+from dotenv import load_dotenv
+import dotenv
+import requests
+
+load_dotenv()
+
+WEBHOOK = str(os.environ["CUSTOMCONNSTR_WEBHOOK_DEV"])
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    req_body = req.get_json()
+    name = req_body.get('name')
+    message = req_body.get('text')
+    msg = f'{name}: {message}'
+    attachments = req_body.get('attachments')
+    attachmentUrl = ''
+    if attachments:
+        attachmentUrl = str(attachments[0].get('url'))
+    if attachmentUrl == 'None':
+        attachmentUrl = ''
+
+    if name == 'discord-pusher':
+        return func.HttpResponse('was a bot message')
+
+    data = { "content": f'{msg} {attachmentUrl}' }
+    # data = { "content": msg, "embed": { "image": { "url": attachmentUrl } } }
+
+    response = requests.post(WEBHOOK, json = data)
