5d0d5b883d626192148ed98edd8fcca667ffb2e1
Tyler Stephenson <tylerstephenson@Tylers-Mac-Pro.local>
2023-01-27 19:55:37 -0500

    convert to service

---
+++ groupme-listener/bot.py
@@ -0,0 +1,34 @@
+import logging
+import webbrowser
+import azure.functions as func
+import os
+from dotenv import load_dotenv
+import dotenv
+import requests
+
+load_dotenv()
+
+WEBHOOK = str(os.environ["WEBHOOK_DEV"])
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    req_body = req.get_json()
+    name = req_body.get('name')
+    message = req_body.get('text')
+    msg = f'**{name}**: {message}'
+    attachments = req_body.get('attachments')
+    attachmentUrl = ''
+    if attachments:
+        attachmentUrl = str(attachments[0].get('url'))
+        logging.info(str(req_body))
+    if attachmentUrl == 'None':
+        attachmentUrl = ''
+
+    if name == 'discord-pusher-dev':
+        return func.HttpResponse('was a bot message')
+
+    data = { "content": f'{msg} {attachmentUrl}' }
+    # data = { "content": msg, "embed": { "image": { "url": attachmentUrl } } }
+
+    response = requests.post(WEBHOOK, json = data)
